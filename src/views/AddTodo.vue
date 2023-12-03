e3d6a53d83863dc4a9a1dc88b82e399ef137ced0
Teguera <aboubacarteguera21@example.com>
2023-12-03 01:39:25 +0000

    debut du frontent et affichage et modification des tâches implementé

---
+++ src/views/AddTodo.vue
@@ -0,0 +1,70 @@
+<template>
+    <div>
+        <form @submit.prevent="addTodo()">
+            <div class="author">
+                <label for="author">Auteur</label>
+                <input v-model="newTodo.author" type="text">
+            </div>
+            <div class="todo">
+                <label for="todo">Todo</label>
+                <input v-model="newTodo.todo" type="text">
+            </div>
+            <input type="submit" value="Enregistrer">
+        </form>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const newTodo = ref([
+    {
+        todo : '',
+        author : ''
+    }
+]);
+
+const addTodo = async () => {
+    let response = await fetch('http://localhost:2100/api/addTodo',{
+        method : "POST",
+        headers: {
+        "Content-Type": "application/json",
+        },
+        body : JSON.stringify({
+            todo : newTodo.value.todo,
+            author : newTodo.value.author
+        }),
+    },).then((res) => res.json())
+    .catch((err) => {
+        console.log(err);
+    });
+
+    console.log(response);
+}
+
+
+</script>
+
+<style scoped>
+
+form , form .author, form .todo{
+    display: flex;
+    flex-direction: column;
+    max-width: 300px;
+}
+
+form input{
+    padding: 7px;
+    border-radius: 5px;
+} 
+
+form div{
+    padding: 15px;
+}
+
+form input[type='submit']{
+    cursor: pointer;
+    margin: auto;
+}
+
+</style>
---
+++ src/views/TodoView.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="todo" v-for="item in todo" :key="todo._id">
+        <h3>{{ item.author }}</h3>
+        <p>{{ item.todo }}</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+const todo = ref([]);
+
+onMounted(async() => {
+    let response = await fetch('http://localhost:2100/api/getTodo')
+    .then((res) => res.json())
+    .catch((err) =>{
+        console.log(err);
+    });
+    todo.value = response;
+    
+});
+
+</script>
+
+<style scoped>
+.todo{
+    margin-top: 50px;
+}
+</style>
